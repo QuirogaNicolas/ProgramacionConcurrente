31b90947916fe0c55ba2935daf08f69fcb3d2cad
unknown <nicolas.quiroga2001@gmail.com>
2021-12-14 20:46:26 -0300

    Ahora la cantidades se pueden ingresar

--- src/Recuperatorio/Tester.java
+++ src/Recuperatorio/Tester.java
@@ -1,18 +1,22 @@
 package Recuperatorio;
 
+import parcial2_Quiroga_Fai_2978.Ejercicio1.TecladoIn;
+
 public class Tester {
     public static void main(String[] args) {
         System.out.println("Ingrese la cantidad de capitulos que tendra la serie");
-        int cantCapitulos = 10;
+        int cantCapitulos = TecladoIn.readInt();
         Plataforma laPlataforma = new Plataforma(cantCapitulos);
         Thread laFilmadora = new Thread(new Filmador(laPlataforma, cantCapitulos), "la Filmadora");
         Thread Traductor1 = new Thread(new Traductor(laPlataforma), "traductor 1");
         Thread Traductor2 = new Thread(new Traductor(laPlataforma), "traductor 2");
-        Thread socios[] = new Thread[5];
-        for (int i = 0; i < 5; i++) {
+        System.out.println("Ingrese la cantidad de socios que habrá");
+        int cantSocios = TecladoIn.readInt();
+        Thread socios[] = new Thread[cantSocios];
+        for (int i = 0; i < cantSocios; i++) {
             socios[i] = new Thread(new Socio(laPlataforma, cantCapitulos), "socio " + i);
         }
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < cantSocios; i++) {
             socios[i].start();
         }
         laFilmadora.start();
