841cc6d8451c3e77b3b5d9de87027b41a75518e5
Nico <nicolas.quiroga2001@gmail.com>
2025-01-30 22:40:44 -0300

    ...

--- src/TrabajoFinalAeropuerto/Pasajero.java
+++ src/TrabajoFinalAeropuerto/Pasajero.java
@@ -1,5 +1,35 @@
 package TrabajoFinalAeropuerto;
 
-public class Pasajero {
+public class Pasajero implements Runnable{
+    //Atributos de los pasajeros
+    private int idPasajero;
+    private String aerolineaPasajero;
+    private String vuelo;
+    private String estadoPasajero;
+    private int terminalAsignada;
+    private int puestoEmbarque;
+    private int puestoAtencion;
     
+    //Nuevo pasajero
+    public Pasajero(int id, String aerolinea, String vuelo){
+        this.idPasajero = id;
+        this.aerolineaPasajero = aerolinea;
+        this.vuelo = vuelo;
+    }
+
+
+    @Override
+    public void run() {
+        //Visita PI
+        puestoAtencion
+
+        //Visita PA
+
+        //Visita Tren
+
+        //Visita FreeShop
+
+        //Embarque
+    }
+
 }
