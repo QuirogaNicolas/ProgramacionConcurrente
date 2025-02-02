77753e1946d6d0ac7b1feac8dd62321891f21ff6
Nico <nicolas.quiroga2001@gmail.com>
2025-02-02 01:56:38 -0300

    empiezo a darle forma al pasajero

---
+++ src/TrabajoFinalAeropuerto/Aeropuerto.java
@@ -0,0 +1,17 @@
+package TrabajoFinalAeropuerto;
+
+public class Aeropuerto {
+    public static void main(String[] args){
+        int horarioAtencion = 2;
+        //Se crean los procesos
+        //Pasajero p1 = new Pasajero(...)
+
+        //Se crean los hilos
+        //Thread h1 = new Thread(p1)
+
+        //Se inician
+        //h1.start()
+
+        //Cierra el aeropuerto
+    }
+}
--- src/TrabajoFinalAeropuerto/Pasajero.java
+++ src/TrabajoFinalAeropuerto/Pasajero.java
@@ -3,12 +3,13 @@ package TrabajoFinalAeropuerto;
 public class Pasajero implements Runnable{
     //Atributos de los pasajeros
     private int idPasajero;
-    private String aerolineaPasajero;
+    private String aerolineaPasajero; //capaz aerolinea está de más
     private String vuelo;
     private String estadoPasajero;
     private int terminalAsignada;
-    private int puestoEmbarque;
-    private int puestoAtencion;
+    private PuestoInformes puestoInforme;
+    private PuestoAtencion puestoAtencion;
+    private Tren tren;
     
     //Nuevo pasajero
     public Pasajero(int id, String aerolinea, String vuelo){
@@ -17,17 +18,35 @@ public class Pasajero implements Runnable{
         this.vuelo = vuelo;
     }
 
+    public void asignarPA(PuestoAtencion pa){
+        this.puestoAtencion = pa;
+    }
+
+    public void asignarTerminal(int numTerminal){
+        this.terminalAsignada = numTerminal;
+    }
 
     @Override
     public void run() {
         //Visita PI
-        
+        puestoInforme.solicitarGuia(vuelo); //El hilo puesto de informes le va a asignar un puesto de atención y aerolinea según el vuelo que tenga
 
         //Visita PA
+        puestoAtencion.hacerCheckIn(vuelo, aerolineaPasajero);; //El hilo puesto de atención le va a asignar una terminal 
 
         //Visita Tren
+        tren.abordar();
+        //se sube
+        tren.bajar(); //El tren le debe avisar en qué terminal está y el pasajero comparar con la terminal que se le asigno
 
         //Visita FreeShop
+        if((int)(Math.random() * 2) == 1){
+            //Tiene ganas de entrar al freeshop
+
+            
+        } else{
+            //No tiene ganas de entrar al freeshop y solamente espera
+        }
 
         //Embarque
     }
